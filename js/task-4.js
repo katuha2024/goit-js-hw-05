c5768bcf6c7efc3e2acfcac289b5176a7c73107e
katuha2024 <ukrbrend2020@gmail.com>
2025-01-06 14:09:32 +0200

    fixedd task-3.js

--- js/task-4.js
+++ js/task-4.js
@@ -44,3 +44,4 @@ const getTotalBalanceByGender = (users, gender) => {
 console.log(getTotalBalanceByGender(clients, "male")); // 12053
 
 console.log(getTotalBalanceByGender(clients, "female")); // 8863
+
